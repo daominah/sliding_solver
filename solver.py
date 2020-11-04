4dc966996cb2151b86581c37300a441469024bd9
Đào Thanh Tùng <tung.dao@techx.vn>
2020-11-04 12:19:22 +0700

    wip try BFMatcher

--- solver.py
+++ solver.py
@@ -118,82 +118,60 @@ class SlidingSolver2Background:
         return diffX, pLeftX
 
 
-if False:
+if __name__ == '__main__':
     cv = cv2
     import matplotlib.pyplot as plt
 
     img1Gray = cv2.imread("/home/tungdt/Desktop/test14_order.png", cv2.IMREAD_GRAYSCALE)
     img2Gray = cv2.imread("/home/tungdt/Desktop/test11_bg.png", cv2.IMREAD_GRAYSCALE)
 
-    img1 = CalcImageEdge(img1Gray)
-    img2 = CalcImageEdge(img2Gray)
+    # img1, img2 = img1Gray, img2Gray
+    # img1, img2 = cv2.GaussianBlur(img1Gray, (3, 3), 0), cv2.GaussianBlur(img2Gray, (3, 3), 0)
+    img1, img2 = CalcImageEdge(img1Gray), CalcImageEdge(img2Gray)
 
-    if True:  # method 1 Brute-Force
-        MIN_MATCH_COUNT = 1
-
-        # Initiate SIFT detector
+    # keyPoints and descriptors result from a feature detector
+    kp1, des1, kp2, des2 = None, None, None, None
+    if True:
         sift = cv.SIFT_create()
-        # find the keypoints and descriptors with SIFT
-        kp1, des1 = sift.detectAndCompute(img1,None)
-        kp2, des2 = sift.detectAndCompute(img2,None)
-        FLANN_INDEX_KDTREE = 1
-        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-        search_params = dict(checks = 50)
-        flann = cv.FlannBasedMatcher(index_params, search_params)
-        matches = flann.knnMatch(des1,des2,k=2)
-        # store all the good matches as per Lowe's ratio test.
-        good = []
-        for m,n in matches:
-            if m.distance < 0.8*n.distance:
-                good.append(m)
+        kp1, des1 = sift.detectAndCompute(img1, None)
+        # imgDebug = cv.drawKeypoints(img1, kp1, None, color=(0, 255, 0), flags=0)
+        # cv2.imshow("", imgDebug); cv2.waitKey()
+        kp2, des2 = sift.detectAndCompute(img2, None)
+    else:
+        orb = cv.ORB_create()
+        kp1 = orb.detect(img1, None)
+        kp1, des1 = orb.compute(img1, kp1)
+        imgDebug = cv.drawKeypoints(img1, kp1, None, color=(0, 255, 0), flags=0)
+        cv2.imshow("", imgDebug); cv2.waitKey()
+        kp2 = orb.detect(img2, None)
+        kp2, des2 = orb.compute(img2, kp2)
 
-        if len(good)>MIN_MATCH_COUNT:
-            src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-            dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
-            M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC,5.0)
-            matchesMask = mask.ravel().tolist()
-            h,w = img1.shape
-            pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-            dst = cv.perspectiveTransform(pts,M)
-            img2 = cv.polylines(img2,[np.int32(dst)],True,255,3, cv.LINE_AA)
-        else:
-            print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
-            matchesMask = None
+    bf = cv.BFMatcher()
+    matches = bf.knnMatch(des1, des2, k=2)
 
-        draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-                           singlePointColor = None,
-                           matchesMask = matchesMask, # draw only inliers
-                           flags = 2)
+    # store all the good matches as per Lowe's ratio test.
+    good = []
+    for m, n in matches:
+        if m.distance < 0.8*n.distance:
+            good.append(m)
 
-        draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-                           singlePointColor = None,
-                           matchesMask = matchesMask, # draw only inliers
-                           flags = 2)
-        img3 = cv.drawMatches(img1Gray,kp1,img2Gray,kp2,good,None,**draw_params)
-        plt.imshow(img3, 'gray'),plt.show()
+    MIN_MATCH_COUNT = 4
+    if len(good) > MIN_MATCH_COUNT:
+        src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+        dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+        M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
+        matchesMask = mask.ravel().tolist()
+        h, w = img1.shape
+        pts = np.float32([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]).reshape(-1, 1, 2)
+        dst = cv.perspectiveTransform(pts, M)
+        img2 = cv.polylines(img2, [np.int32(dst)], True, 255, 3, cv.LINE_AA)
+    else:
+        print("not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
+        matchesMask = None
 
-    if False:
-        # Initiate SIFT detector
-        sift = cv.SIFT_create()
-        # find the keypoints and descriptors with SIFT
-        kp1, des1 = sift.detectAndCompute(img1,None)
-        kp2, des2 = sift.detectAndCompute(img2,None)
-        # FLANN parameters
-        # FLANN parameters
-        FLANN_INDEX_KDTREE = 1
-        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-        search_params = dict(checks=50)   # or pass empty dictionary
-        flann = cv.FlannBasedMatcher(index_params,search_params)
-        matches = flann.knnMatch(des1,des2,k=2)
-        # Need to draw only good matches, so create a mask
-        matchesMask = [[0,0] for i in range(len(matches))]
-        # ratio test as per Lowe's paper
-        for i,(m,n) in enumerate(matches):
-            if m.distance < .8*n.distance:
-                matchesMask[i]=[1,0]
-        draw_params = dict(matchColor = (0,255,0),
-                           singlePointColor = (255,0,0),
-                           matchesMask = matchesMask,
-                           flags = cv.DrawMatchesFlags_DEFAULT)
-        img3 = cv.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
-        plt.imshow(img3,),plt.show()
+    draw_params = dict(matchColor=(0, 255, 0),
+                       singlePointColor=(255, 0, 0),
+                       matchesMask=matchesMask,  # draw only inliers
+                       flags=2)
+    img3 = cv.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
+    plt.imshow(img3, 'gray'), plt.show()
